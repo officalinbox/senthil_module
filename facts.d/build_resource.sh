b8ee2e922b3f69373c855b289b12e0a3d5606240
SenthilNathanA <chatwithnathan@gmail.com>
2026-07-18 16:47:23 -0400

    fact

--- facts.d/build_resource.sh
+++ facts.d/build_resource.sh
@@ -2,4 +2,4 @@
 old=$(ls -rt /var/log/wtmp* |head -1)
 #echo "oldest file: $old"
 user=$(last -F -f "$old" |tac |head -4 |egrep -v "addm_gt|reboot|wtmp" |awk '{print $1}')
-echo $user
+echo "Build_Resource=${user}"
